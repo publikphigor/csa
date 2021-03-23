fc513d8ab93e2d04b2113a1660f5b8ca43caf75c
Koladele Olaitan <koladeleolaitan@gmail.com>
2021-03-23 19:58:24 +0100

    updates

---
+++ js/dashboard.js
@@ -0,0 +1,43 @@
+const allCourseBtn = document.querySelector('.c-1');
+const compCourseBtn = document.querySelector('.c-2');
+const favCourseBtn = document.querySelector('.c-3');
+const allCourse = document.querySelector('.all-c');
+const compCourse = document.querySelector('.comp-c');
+const favCourse = document.querySelector('.fav-c');
+
+allCourseBtn.onclick = ()=>{
+    allCourseBtn.classList.add('active');
+    compCourseBtn.classList.remove('active');
+    favCourseBtn.classList.remove('active');
+    allCourse.classList.remove('d-none');
+    compCourse.classList.add('d-none');
+    favCourse.classList.add('d-none');
+};
+
+compCourseBtn.onclick = ()=>{
+    compCourseBtn.classList.add('active');
+    allCourseBtn.classList.remove('active');
+    favCourseBtn.classList.remove('active');
+    compCourse.classList.remove('d-none');
+    allCourse.classList.add('d-none');
+    favCourse.classList.add('d-none');
+};
+
+favCourseBtn.onclick = ()=>{
+    favCourseBtn.classList.add('active');
+    compCourseBtn.classList.remove('active');
+    allCourseBtn.classList.remove('active');
+    favCourse.classList.remove('d-none');
+    compCourse.classList.add('d-none');
+    allCourse.classList.add('d-none');
+};
+
+
+//navbar slide out
+const navbar = document.querySelector('.sidebar');
+const burger = document.querySelector('.burger');
+
+function showNav(navbar) {
+    navbar.classList.toggle('active');
+    burger.classList.toggle('active');
+};
