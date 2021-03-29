7bca1731d5030d3b17e724913592ba4ec578227d
Koladele Olaitan <koladeleolaitan@gmail.com>
2021-03-29 13:22:34 +0100

    update

---
+++ js/inbox.js
@@ -0,0 +1,29 @@
+const msgList = document.querySelectorAll('.ibx-li');
+const msgArea = document.querySelector('.ibx-msg-ex');
+
+const openMessage = ()=>{
+    msgList.forEach(message => {
+        message.addEventListener('click', (e)=>{
+            clearActive();
+            if(e.target.classList.contains('ibx.li')) {
+                e.target.classList.add('active');
+            } else {
+                e.target.closest('.ibx-li').classList.add('active');
+            }
+
+            let username = message.querySelector('.ibx-m-name').innerText;
+            let msgContent = message.querySelector('.ibx-msg').innerText;
+
+            msgArea.querySelector('h3').innerText = `Dear ${username},`;
+            msgArea.querySelector('p').innerText = msgContent;
+        });
+    });
+};
+
+openMessage();
+
+function clearActive() {
+    msgList.forEach(message =>{
+        message.classList.remove('active');
+    });
+};
